c27adeca617aaba607763a699ee0b5f624bd5fa4
Victoria Louise Steinwachs <69801019+vilouspics@users.noreply.github.com>
2024-07-12 16:49:08 +0200

    Improved UX

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ function displayPoem(response) {
       });
 }
 
-
 function generatePoem(event) {
     event.preventDefault();
 
@@ -17,6 +16,10 @@ function generatePoem(event) {
     let prompt = `Generate a French poem about ${instructionsInput.value}`;
     let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a four line poem in basic HTML while breaking every line with </br>. Do not include a title to the poem. Make sure to follow the user instructions. Sign with 'SheCodes AI' inside a <strong> element at the end of the poem.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="blink-soft">Generating a French ${instructionsInput.value} poem for you...</div>`;
 
     axios.get(apiURL).then(displayPoem);
 
